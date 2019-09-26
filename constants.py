9c54f26cddaf8696968b5f7705332b9e1a786df8
Ubuntu <ubuntu@ip-172-31-28-78.ec2.internal>
2019-09-26 21:18:59 +0000

    commit

---
+++ constants.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env pythone3
+
+# created by: Andrei Chirilov
+# created on: Spetember 2019
+# This module contains constants
+
+
+LABOR = 0.75
+RENT = 1.00
+COST_PER_INCH = 0.50
+HST = 0.13
